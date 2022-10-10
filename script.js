595740d413c3223df4e861e1ed280de50f134e60
apple <wonderfulaya.2020@gmail.com>
2022-10-10 01:36:22 -0700

    ニュース追加

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(".slide-items").slick({
-    autoplay: true, // 自動再生
-    autoplaySpeed: 3000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
-    infinite: true, // 無限スライド
-    cssEase: 'linear',// アニメーション
-    speed: 600, // フェードアニメーションの速度設定
-    fade: true, // スライドをフェードイン・フェードアウト
+  autoplay: true, // 自動再生
+  autoplaySpeed: 3000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
+  infinite: true, // 無限スライド
+  cssEase: 'linear',// アニメーション
+  speed: 600, // フェードアニメーションの速度設定
+  fade: true, // スライドをフェードイン・フェードアウト
 
-    arrows: true, // 矢印
-    dots: true, // インジケーター
+  arrows: true, // 矢印
+  dots: true, // インジケーター
 });
 
 
 $('#ham-btn').on('click', function () {
-    $(this).toggleClass('on');
-  })
+  $(this).toggleClass('on');
+})
 
 
 
@@ -99,30 +99,35 @@ $('#ham-btn').on('click', function () {
 
 // あやのjsはここから
 // ドロップダウン
-$(function() {
-    $("ul li").hover(
-      function() {
-        $("ul:not(:animated)", this).slideDown();
-      },
-      function() {
-        $("ul", this).slideUp();
-      }
-    );
-    // マウスが重なったらホバー
-    $('.icon').on('mouseover',function () {
-        $(this).css('opacity',0.5);
-    }).on('mouseleave',function(){
-        $(this).css('opacity',1);
-    })
-    $('.nomal').on('mouseover',function () {
-        $(this).css('opacity',0.5);
-    }).on('mouseleave',function(){
-        $(this).css('opacity',1);
-    })
-    $('.ctg').on('mouseover',function () {
-        $(this).css('opacity',0.5);
-    }).on('mouseleave',function(){
-        $(this).css('opacity',1);
-    })
-   
-  });
+$(function () {
+  $("ul li").hover(
+    function () {
+      $("ul:not(:animated)", this).slideDown();
+    },
+    function () {
+      $("ul", this).slideUp();
+    }
+  );
+  // マウスが重なったらホバー カテゴリー
+  $('.icon').on('mouseover', function () {
+    $(this).css('opacity', 0.5);
+  }).on('mouseleave', function () {
+    $(this).css('opacity', 1);
+  })
+  $('.nomal').on('mouseover', function () {
+    $(this).css('opacity', 0.5);
+  }).on('mouseleave', function () {
+    $(this).css('opacity', 1);
+  })
+  $('.ctg').on('mouseover', function () {
+    $(this).css('opacity', 0.5);
+  }).on('mouseleave', function () {
+    $(this).css('opacity', 1);
+  })
+  // 新着アイテム　ホバー
+  $('.newarrival1').on('mouseover', function () {
+    $(this).css('opacity', 0.7);
+  }).on('mouseleave', function () {
+    $(this).css('opacity', 1);
+  })
+});
