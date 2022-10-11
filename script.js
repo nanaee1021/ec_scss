b8d7296b37807c00f62042343d0287a2fbfd8e1e
nanae <7a1021@gmail.com>
2022-10-11 14:22:11 +0900

    tabu

--- script.js
+++ script.js
@@ -16,7 +16,17 @@ $('#ham-btn').on('click', function () {
 })
 
 
+$('#ham-btn').on('click', function () {
+  $('.ham-btn, .nav').toggleClass('show');
+});
 
+
+$('.tab_box .tab_btn').click(function() {
+	var index = $('.tab_box .tab_btn').index(this);
+	$('.tab_box .tab_btn, .tab_box .tab_panel').removeClass('active');
+	$(this).addClass('active');
+	$('.tab_box .tab_panel').eq(index).addClass('active');
+});
 
 
 
